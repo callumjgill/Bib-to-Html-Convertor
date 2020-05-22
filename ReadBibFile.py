d0b7653b7fe08aa447106674b78bc891b8aa6c16
Callum Gill <callum.j.gill@googlemail.com>
2020-05-22 10:35:20 +0100

    Update ReadBibFile.py and config.properties
    ReadBibFile.py now includes a function to read each entry for its
    entry type and fields. Entry type is read sucessfully, nothing for
    reading each field yet.

--- ReadBibFile.py
+++ ReadBibFile.py
@@ -31,6 +31,22 @@ def readBibEntries(bib_file):
     del ref_list[0] # First item in list is blank due to the .split function
     return ref_list
 
+def readEntry(bib_entry):
+    """
+    Reads the .bib entry and returns a tuple for the entries
+
+    Parameters:
+            bib_entry : string
+                the entry in the .bib file which has been stored as a string
+    """
+    # Seperate the entry into entry type and fields
+    temp_list = bib_entry.split(",", 1)
+    entry_type = temp_list[0]
+    fields_list = temp_list[1]
+    # Format the entry type string to just be the entry type name
+    entry_type = entry_type.split("{", 1)[0]
+    
+
 # PUBLIC CLASSES
 class reference:
     "Class corresponding to an entry in a .bib file"
@@ -38,14 +54,18 @@ class reference:
 
     def __init__(self, bib_entry):
         """
-
         Constructor for reference object. Assigns all fields in .bib entry into
         class attributes
 
+        parameters:
+            bib_entry : string
+                the entry in the .bib file which has been stored as a string
+
         """
-        #self.entry_type = 
+        #self.entry_type =
 
 # TESTING
 filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), config_parser.get("Directory Paths", "bib_path"), "reference.bib")
 bib_file = open(filename, "r")
 entries = readBibEntries(bib_file)
+readEntry(entries[0])
