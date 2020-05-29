f65b721147d8ce1640f83393457ba2c71015eac5
Callum Gill <callum.j.gill@googlemail.com>
2020-05-29 16:10:12 +0100

    Add readEntry() and checkField() functions
    These functions store the fields of each entry into a dictionary and
    checks if the field types are valid or not.

--- ReadBibFile.py
+++ ReadBibFile.py
@@ -9,10 +9,13 @@ Description: Takes a .bib file as input and stores the relevant information
 import os
 import configparser
 
+
 # Read the config file
-config_parser = configparser.RawConfigParser()
+CONFIG_PARSER = configparser.RawConfigParser()
 config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.properties")
-config_parser.read(config_path)
+CONFIG_PARSER.read(config_path)
+# All valid field types from config file
+VALID_FIELDS = dict(CONFIG_PARSER.items("Bib Field Types"))
 
 # FUNCTIONS
 def readBibEntries(bib_file):
@@ -23,6 +26,10 @@ def readBibEntries(bib_file):
         Parameters:
             file : file object
                 the .bib file being read. Must be a .bib file otherwise an AssertError exception is made
+
+        Returns:
+            ref_list : list of strings
+                each item in the list is a string corresponding to a single entry in the .bib file
     """
     ext = bib_file.name.rpartition(".")[-1]
     assert ext == "bib", r"%s is not a .bib file!" % bib_file.name # Call an AssertError if file isn't a .bib file
@@ -33,18 +40,62 @@ def readBibEntries(bib_file):
 
 def readEntry(bib_entry):
     """
-    Reads the .bib entry and returns a tuple for the entries
+    Reads the .bib entry and returns a dictionary for the entries
 
     Parameters:
             bib_entry : string
                 the entry in the .bib file which has been stored as a string
+
+    Returns:
+        fields : dictionary
+            keys in the dictionary are the field types and the corresponding
+            values are the content
     """
     # Seperate the entry into entry type and fields
     temp_list = bib_entry.split(",", 1)
     entry_type = temp_list[0]
-    fields_list = temp_list[1]
+    fields_list_str = temp_list[1].replace("\n", "").replace("\t", "") # single string storing all fields, also replaces irrelevant characters
     # Format the entry type string to just be the entry type name
     entry_type = entry_type.split("{", 1)[0]
+    # Format the fields into a list and then into a dictionary
+    # First it replaces the string '},' whereever it appears with '",'
+    # Then it splits at '",". This will then correctly get each field entry as you can either enclose them in " " or { }
+    fields_list = fields_list_str.replace('},', '",').split('",')
+    fields = {}
+    # Now the field name and content is split up
+    for field in fields_list:
+        key_value_list = field.split("=")
+        key = key_value_list[0].replace(" ", "")
+        # checks if the field type is valid
+        is_key_valid = checkField(key)
+        assert is_key_valid, r"%s is not a valid field type!" % key
+        # Removes unnecessary characters from the value
+        value = key_value_list[1].replace("{", "").replace("}", "").replace(' "', "").replace('"', "")
+        # If the field type is author then each author is stored as a list of strings
+        if key == 'author':
+            # Removes whitespace in front of first author name and splits string a the comma. Also removes whitespace in front of other authors if there is any
+            value = value.replace(" ", "", 1).replace(", ", ",").split(",")
+        # Builds the dictionary with each field entry type as a key and the value the content of the field
+        fields[key] = value
+    return fields
+
+def checkField(field):
+    """
+        Checks the field type against the config file for valid field types.
+        
+        Parameters:
+            field : string
+                The field type being tested
+
+        Returns:
+            is_valid : boolean 
+                true if valid, false otherwise
+    """
+    is_valid = False
+    if field in VALID_FIELDS.keys():
+        is_valid = True
+    return is_valid
+
     
 
 # PUBLIC CLASSES
@@ -65,7 +116,7 @@ class reference:
         #self.entry_type =
 
 # TESTING
-filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), config_parser.get("Directory Paths", "bib_path"), "reference.bib")
+filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), CONFIG_PARSER.get("Directory Paths", "bib_path"), "reference.bib")
 bib_file = open(filename, "r")
 entries = readBibEntries(bib_file)
 readEntry(entries[0])
