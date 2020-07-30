efe5f32438698a84fbf6b3042b8dc75a0cedb5b7
Callum Gill <callum.j.gill@googlemail.com>
2020-07-30 11:24:43 +0100

    Add Improved Error messages
    Error messages in BibParser.py now tell you which entry and file the
    error occured in.

--- BibParser.py
+++ BibParser.py
@@ -16,6 +16,8 @@ VALID_ENTRIES = ReadConfig.getSectionItems("Bib Entry Types")
 VALID_FIELDS = ReadConfig.getSectionItems("Bib Field Types")
 REQUIRED_FIELDS = ReadConfig.getSectionItems("Required Fields")
 OPTIONAL_FIELDS = ReadConfig.getSectionItems("Optional Fields")
+# Message global variable for error
+MSG_FILE = "" # Filename is assigned in bibParser
 
 # FUNCTIONS
 def bibParser(bib_file):
@@ -30,11 +32,14 @@ def bibParser(bib_file):
 
         Returns:
             ref_list : list of Reference objects
-                each item in the list is an object corresponding to a single reference entry in the .bib file
+                each item in the list is an object corresponding to a single reference 
+                entry in the .bib file and 
     """
+    global MSG_FILE
     ext = bib_file.name.rpartition(".")[-1]
     if ext != "bib":
         raise ValueError(r"%s is not a .bib file!" % bib_file.name) # Raise a ValueError if file isn't a .bib file
+    MSG_FILE += bib_file.name
     contents = bib_file.read() # Contents of the .bib file
     ref_list_str = contents.split("@") # Creates a list of each entry in the .bib file
     del ref_list_str[0] # First item in list is blank due to the .split function
@@ -82,10 +87,11 @@ class Reference:
         temp_list = bib_entry.split(",", 1)
         entry_type = temp_list[0]
         fields_list_str = temp_list[1].replace("\n", "").replace("\t", "") # single string storing all fields, also replaces irrelevant characters
-        # Format the entry type string to just be the entry type name
-        entry_type = entry_type.split("{", 1)[0]
+        # Format the entry type string to just be the entry type name and the other side of the { char is the entry name
+        entry_type, entry_name = entry_type.split("{", 1)
         if not self.__checkIfValid(entry_type, VALID_ENTRIES):
-            raise ValueError(r"%s is not a valid entry type!" % entry_type)
+            err_msg = entry_type + " is not a valid entry type!\n" + "Entry: " + entry_name + "\nFile: " + MSG_FILE
+            raise ValueError(err_msg)
         # Format the fields into a list and then into a dictionary
         # First it replaces the string '},' whereever it appears with '",'
         # Then it splits at '",". This will then correctly get each field entry as you can either enclose them in " " or { }
@@ -97,10 +103,12 @@ class Reference:
             key = key_value_list[0].replace(" ", "")
             # checks if the field type is valid
             if not self.__checkIfValid(key, VALID_FIELDS):
-                raise ValueError(r"%s is not a valid field type!" % key)
+                err_msg = key + " is not a valid field type!\n" + "Entry: " + entry_name + "\nFile: " + MSG_FILE
+                raise ValueError(err_msg)
             # checks if the field type can actually be used with the entry type
             if not self.__checkField(entry_type, key):
-                raise ValueError(r"%s isn't a recognised field type for the entry type %s!" % (key, entry_type))
+                err_msg = key + " isn't a recognised field type for the entry type " + entry_type + "!\nEntry: " + entry_name + "\nFile: " + MSG_FILE
+                raise ValueError(err_msg)
             # Removes unnecessary characters from the value
             value = key_value_list[1].replace("{", "").replace("}", "").replace(' "', "").replace('"', "").replace(" \\", "")
             # If the field type is author or editor then each author/editor is stored as a list of strings
@@ -122,7 +130,8 @@ class Reference:
         if entry_type != "misc":
             required_fields_given = [item for item in fields if item in REQUIRED_FIELDS[entry_type]]
             if collections.Counter(required_fields_given) != collections.Counter(REQUIRED_FIELDS[entry_type]):
-                missing_msg = r"Missing required fields!\n Fields given: %s \n Fields required: %s" % (", ".join(fields.keys()), ", ".join(REQUIRED_FIELDS[entry_type]))
+                missing_msg = r"Missing required fields!\n Fields given: %s \n Fields required: %s \n" % (", ".join(fields.keys()), ", ".join(REQUIRED_FIELDS[entry_type]))
+                err_msg = repr(missing_msg).replace("\\n", "\n") + "Entry: " + entry_name + "\nFile: " + MSG_FILE + "\nLines: "
                 raise ValueError(repr(missing_msg).replace("\\n", "\n"))
         return (entry_type, fields)
 
